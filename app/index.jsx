226372d94a40da00ede9850ee610a10d03946daf
chndnnn <chndn136@gmail.com>
2024-10-28 18:26:17 +0530

    added header section

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,44 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { TextInput, TouchableOpacity } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
+import { MagnifyingGlassIcon as SearchIconOutline } from "react-native-heroicons/outline";
+import { Bars3BottomLeftIcon, XMarkIcon } from "react-native-heroicons/outline";
 
 const Home = () => {
+  const [searchClicked, setSearchClicked] = useState(false);
   return (
-    <View>
-      <Text className="text-red-500">Home</Text>
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className="flex-1 bg-neutral-800 mt-10 p-1">
+        <View>
+          {!searchClicked ? (
+            <View className="flex flex-row justify-between p-2 items-cente">
+              <Bars3BottomLeftIcon color="white" />
+              <View className="flex-row items-center">
+                <Text className="text-yellow-500 text-3xl">M</Text>
+                <Text className="text-white text-2xl font-bold">ovie</Text>
+              </View>
+              <TouchableOpacity onPress={() => setSearchClicked(true)}>
+                <SearchIconOutline color="white" />
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <View className="flex flex-row justify-between p-2 items-center ">
+              <TextInput
+                placeholder="search"
+                className="text-white border border-solid border-neutral-500 w-[80%] px-2 rounded"
+              />
+              <View className="flex flex-row gap-2">
+                <TouchableOpacity>
+                  <SearchIconOutline color="white" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => setSearchClicked(false)}>
+                  <XMarkIcon color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        </View>
+      </SafeAreaView>
     </View>
   );
 };
